ce6573a4f9fac8f8e0bcbb3df18e0702f85b31bb
Joao Amaral <joaopcamaral@gmail.com>
2021-03-04 00:55:31 +0000

    tokenTest
    tokenTest

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
+from dotenv import load_dotenv
+import os
+from os.path import join,dirname
+dotenv_path = join(dirname(__file__),'.env')
+load_dotenv(dotenv_path)
+TOKEN_KEY = os.environ.get("TOKEN")
+print(TOKEN_KEY)
+
 print("Hello Bruno")
 print("Amaral")
